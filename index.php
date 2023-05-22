fb7da17dbebfc2bf1870fd02f9542004c908065a
miftahr149 <miftahr149@gmail.com>
2023-05-22 08:11:49 +0700

    adding some stuff

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/index.css" rel=stylesheet>
+    <title>PHP Project</title>
+</head>
+
+<body class="">
+    <?php include('template/header.html') ?>
+    
+    <main class="main">
+        Main
+    </main>
+
+    <footer class="footer">
+        Footer
+    </footer>
+</body>
+
+</html>
