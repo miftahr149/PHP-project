6baf99a34608ab83533e518dbca8d5a784b91fd2
miftahr149 <miftahr149@gmail.com>
2023-06-27 12:25:20 +0700

    working on background image in profile page

--- view/profile.php
+++ view/profile.php
@@ -1,5 +1,6 @@
 <?php
 include("../template/home-head.php");
+editMethod();
 $profileData = getProfileData();
 ?>
 <link rel="stylesheet" href="../css/profile.css?v=<?php echo time(); ?>">
@@ -10,21 +11,21 @@ $profileData = getProfileData();
     <?php include("../template/header.php") ?>
 
     <main class="main flex-box flex-grow">
-        <div class="user-profile flex-box box--padding">
+        <div class="user-profile flex-box box--padding" style="<?php getBackground($profileData['image_background']); ?>">
             <div class="user-profile__left flex-grow">
                 <p class="username"><?php echo $profileData['username'] ?></p>
             </div>
             <div class="user-profile__right flex-box flex-grow">
-                <?php changeBackground($profileData) ?>
+                <?php editProfile($profileData) ?>
             </div>
         </div>
 
         <div class="edit-window-wrapper flex-box flex-center none">
             <div class="edit-window flex-box">
-                <div class="edit__header">
+                <div class="edit__header edit-window--padding">
                     <button class="back-button" onclick="editFunction()"></button>
                 </div>
-                <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post" class="edit flex-grow">
+                <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data" class="edit edit-window--padding flex-grow">
                     <div class="flex-box" style="gap: 1.5rem">
                         <div class="flex-grow">
                             <label for="firstName" class="block">First Name</label>
@@ -37,8 +38,8 @@ $profileData = getProfileData();
                     </div>
 
                     <div class="edit--margin-top" style="margin-top: 1rem">
-                        <label for="backgroundImage">Background Image </label>
-                        <input type="file" name="backgroundImage" class="edit__input" value="<?php $profileData['background_image'] ?>">
+                        <label for="imageBackground">Background Image </label>
+                        <input type="file" name="imageBackground" class="edit__input" value="<?php $profileData['image_background'] ?>">
                     </div>
 
                     <div class="flex-box edit--margin-top" style="gap: 1.5rem;">
@@ -91,18 +92,56 @@ function getProfileData(): array
     return $profileData;
 }
 
-function getBackground(string $background): string | null
+function getBackground(string $background): void
 {
-    if (empty($background)) return null;
+    $text = "background-size: cover;
+             background-position: center;";
+    if (empty($background)) {
+        echo
+        "
+        background: linear-gradient(90deg, rgba(5, 5, 6, 1) 0%, rgba(39, 39, 50, 1) 49%, rgba(79, 79, 79, 1) 87%);
+        " . $text;
+        return;
+    };
+
+    echo $text .
+    "
+    background: url('$background');
+    ". $text;
 }
 
-function changeBackground(array $profileData): void
+function editProfile(array $profileData): void
 {
     if ($profileData['username'] !== getUserData('username')) return;
     echo
     '
     <button class="edit-button button button--white-border button--white-hover" onclick="editFunction()">Edit</button>
     ';
+}
+
+function editMethod(): void
+{
+    if ($_SERVER["REQUEST_METHOD"] != "POST") return;
+    if (empty($_FILES['imageBackground']) or 
+        $_FILES['imageBackground']['error'] !== UPLOAD_ERR_OK) return;
+    
+    $file = $_FILES['imageBackground'];
+    $destination = "../uploads/" . getUserData("username") . '.jpg';
+    move_uploaded_file($file['tmp_name'], $destination);
+    $id = getUserData('id');
+
+    extract($_POST);
+    $conn = getConn();
+    $sql = "UPDATE account SET
+            first_name = '$firstName',
+            last_name = '$lastName',
+            bio = '$bio',
+            image_background = '$destination',
+            city = '$city',
+            country = '$country'
+            WHERE id = '$id'";
+    $conn->query($sql);
+    $conn->close();
 }
 
 ?>
